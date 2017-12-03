bdbf23c35acca5185d358066abf6f6a0994f77ff
kritsana <kritsana@gmail.com>
2017-12-03 10:32:59 +0700

    Add Fragment Sucess

--- app/src/main/java/pattayagorn/kritsana/tuexchange/MainActivity.java
+++ app/src/main/java/pattayagorn/kritsana/tuexchange/MainActivity.java
@@ -3,11 +3,28 @@ package pattayagorn.kritsana.tuexchange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import pattayagorn.kritsana.tuexchange.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+//            Condition is True
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }   // if
+
+
+
+    }   // Main Method
+
+}   // Main Class
